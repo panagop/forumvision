b481832035830430994f571990cd06df66e32e8b
panagop <gpanagop@gmail.com>
2022-06-11 13:54:31 +0300

    altair charts

--- pages/01_songs_by_player.py
+++ pages/01_songs_by_player.py
@@ -2,15 +2,46 @@
 import streamlit as st
 import pandas as pd
 from forumvision import df
+import altair as alt
 
 st.markdown("# Songs by player")
 st.sidebar.markdown("## Songs by player")
 
 players = sorted(df['Player'].unique())
 
-selected_player = st.selectbox('Select a player', players)
+# select the player
+selected_player = st.sidebar.selectbox('Select a player', players)
 
-st.markdown(f"### {selected_player}")
+# chart options
+chart_option = st.sidebar.radio('Chart option', ['Points', 'Position'])
+chart_text_option = st.sidebar.radio('Chart text', ['Title', 'Artist'])
 
-st.dataframe(df[df['Player']==selected_player])
+st.markdown(f"### Επιλογή παίκτη: {selected_player}")
 
+# Table
+st.markdown(f"#### τι διάλεξε")
+
+# get the player's songs and display the table
+df_player = df[df['Player']==selected_player]
+st.dataframe(df_player)
+
+# Chart
+st.markdown(f"#### ...και τι κατάφερε")
+
+# prepare the chart
+chart = alt.Chart(df_player, width=600).mark_bar().encode(
+    x=chart_option,
+    y='Game'
+)
+
+text = chart.mark_text(
+    align='left',
+    baseline='middle',
+    color='green',
+    dx=3  # Nudges text to right so it doesn't appear on top of the bar
+).encode(
+    text=str(chart_text_option)
+)
+
+# display the chart
+st.altair_chart((chart+text))
