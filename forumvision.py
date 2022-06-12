957c2422013462ee34e024f1023530c04e0971be
panagop <gpanagop@gmail.com>
2022-06-12 16:47:33 +0300

    Update forumvision.py

--- forumvision.py
+++ forumvision.py
@@ -4,12 +4,18 @@ import pandas as pd
 import os.path
 import gspread
 from gspread_dataframe import get_as_dataframe
+import json
 
 
 # df = pd.read_excel("data/forumvision.xlsx")
 
 if os.path.exists('credentials.json'):
-    sa = gspread.service_account(filename='credentials.json')
+    with open('credentials.json') as f:
+        creds = json.load(f)
+    # st.write(jjj)
+
+    # sa = gspread.service_account(filename='credentials.json')
+    sa = gspread.service_account_from_dict(creds)
     sh =sa.open("forumvision")
     wks = sh.worksheet(title="Sheet1")
     df = get_as_dataframe(wks, usecols=[0,1,2,3,4,5])
@@ -17,6 +23,7 @@ if os.path.exists('credentials.json'):
 else:
     df = pd.read_excel("data/forumvision.xlsx")
     # print('loaded from local file')
+    st.write(st.secrets.creds.to_dict())
 
 def main_page():
     st.markdown("# Forumvision - full table")
