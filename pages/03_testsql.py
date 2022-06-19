a44b0372a063da1f4c2efb73112377d3c2c2da01
panagop <gpanagop@gmail.com>
2022-06-19 17:15:36 +0300

    Create 03_testsql.py

---
+++ pages/03_testsql.py
@@ -0,0 +1,11 @@
+﻿import streamlit as st
+import pandas as pd
+
+from models import Base, Song, Game, Gyros, Player, Grade, GyroComment
+from forumvision import session, engine
+
+
+st.markdown('## Σχόλια γύρων')
+df2 = pd.read_sql(session.query(GyroComment).statement, engine)
+st.dataframe(df2)
+
