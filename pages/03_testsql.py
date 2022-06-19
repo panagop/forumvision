705f404a2afc136ffa408ec60a016785cb767f70
panagop <gpanagop@gmail.com>
2022-06-19 17:28:42 +0300

    AgGrid

--- pages/03_testsql.py
+++ pages/03_testsql.py
@@ -1,11 +1,14 @@
 ﻿import streamlit as st
 import pandas as pd
+from st_aggrid import AgGrid
 
 from models import Base, Song, Game, Gyros, Player, Grade, GyroComment
 from forumvision import session, engine
 
 
-st.markdown('## Σχόλια γύρων')
-df2 = pd.read_sql(session.query(GyroComment).statement, engine)
-st.dataframe(df2)
+st.markdown('## Βαθμοί')
+df2 = pd.read_sql(session.query(Grade).statement, engine)
+# st.write(df2)
+# st.dataframe(df2)
 
+AgGrid(df2)
