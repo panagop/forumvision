9769acf46a40978851c6480bf3aef055b7935152
panagop <gpanagop@gmail.com>
2022-06-25 22:17:19 +0300

    Update forumvision.py

--- forumvision.py
+++ forumvision.py
@@ -1,40 +1,46 @@
-﻿import os.path
-import json
+﻿# import os.path
+# import json
+# import gspread
+# from gspread_dataframe import get_as_dataframe
+import pandas as pd
 import streamlit as st
-import gspread
-from gspread_dataframe import get_as_dataframe
+from st_aggrid import AgGrid, GridUpdateMode, JsCode
+from st_aggrid.grid_options_builder import GridOptionsBuilder
 from sqlalchemy import create_engine
 from sqlalchemy import insert, func, desc
 from sqlalchemy.orm import Session
 from models import Base, Song, Game, Gyros, Player, Grade, GyroComment
 from models import SongRanking, PlayerRanking
-import pandas as pd
-from st_aggrid import AgGrid, GridUpdateMode, JsCode
-from st_aggrid.grid_options_builder import GridOptionsBuilder
+
+
 
 st.set_page_config(page_title="forumvision statistics", layout="centered")
 
-# # Alternative option: Load data from Excel file
-# df = pd.read_excel("data/forumvision.xlsx")
 
-if os.path.exists('credentials.json'):
-    with open('credentials.json') as f:
-        creds = json.load(f)
-    # # or directly from the json file
-    # sa = gspread.service_account(filename='credentials.json')
-else:
-    creds = dict(st.secrets.creds)
+## older version....using google sheets
 
-engine = create_engine('sqlite:///data/forumvision.db', echo=False)
-session = Session(engine)
+# if os.path.exists('credentials.json'):
+#     with open('credentials.json') as f:
+#         creds = json.load(f)
+#     # # or directly from the json file
+#     # sa = gspread.service_account(filename='credentials.json')
+# else:
+#     creds = dict(st.secrets.creds)
 
 # sa = gspread.service_account_from_dict(creds)
 # sh = sa.open("forumvision")
 # wks = sh.worksheet(title="Sheet1")
 # df = get_as_dataframe(wks, usecols=[0, 1, 2, 3, 4, 5])
 
+# end of older version....using google sheets 
 
 
+# # Alternative older version....: Load data from Excel file
+# df = pd.read_excel("data/forumvision.xlsx")
+
+
+engine = create_engine('sqlite:///data/forumvision.db', echo=False)
+session = Session(engine)
 
 query = session.query(Grade.song_id,
                     Song.artist.label('Artist'), 
